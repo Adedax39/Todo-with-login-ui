7a28881ba2ceb0a1fd2f2d8681a1e14fdcc1fcec
Lasath.r <Lasath.r@dmsswe.com>
2024-05-22 16:39:05 +0530

    font end

--- Login ui-React/my-app/src/App.js
+++ Login ui-React/my-app/src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar1 from './Components/Navbar/Navbar';
+import backgroundImage from './images/wp4069431.png';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+<div className='app'>
+  <div className='background'></div>
+  <div className='content-navbar'>
+  <Navbar1 />
+  </div>
+</div>
+
   );
 }
 
--- Login ui-React/my-app/src/Components/Navbar/Navbar.jsx
+++ Login ui-React/my-app/src/Components/Navbar/Navbar.jsx
@@ -1,4 +1,20 @@
-const Navbar = () =>{
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Button from '@mui/material/Button';
+
+const Navbar1 = () =>{
+    return(
+    <Navbar className="bg-body-tertiary">
+      <Container>
+        <Navbar.Brand href="#home" className='Topic'>Todo-App</Navbar.Brand>
+        <Navbar.Toggle />
+        <Navbar.Collapse className="justify-content-end">
+        <Button >Sign in</Button>
+        <Button >Sign up</Button>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+    )
 
 }
-export default Navbar
+export default Navbar1
