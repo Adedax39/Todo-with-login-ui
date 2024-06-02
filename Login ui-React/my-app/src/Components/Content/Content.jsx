3b17928831e546ade4fb368c08573da5323f70c5
Lasath.r <Lasath.r@dmsswe.com>
2024-06-02 22:29:10 +0530

    Login page showing up

--- Login ui-React/my-app/src/Components/Content/Content.jsx
+++ Login ui-React/my-app/src/Components/Content/Content.jsx
@@ -3,10 +3,16 @@ import Box from '@mui/material/Box';
 import './Content.css'
 import Button from 'react-bootstrap/Button';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { useState } from 'react';
+import SignUpForm from '../Login/Login.jsx';
 
 export default function BoxSystemProps() {
+    const[showSignUp,setShowSignUp] = useState(false);
+    const handleShow = () => setShowSignUp(true);
+    const handleClose = () => setShowSignUp(false);
+
   return (
-    <div className="MuiBox-root">
+    <div className={`MuiBox-root ${showSignUp ? 'blur-background' : ''}`}>
     <h1 className='success'>Success</h1>
      <p className='message'>consists of going from failure to failure without loss of enthusiasm.</p>  
      <br/>
@@ -16,9 +22,10 @@ export default function BoxSystemProps() {
      <br/>
      <br/>
      <br/>
-     <a href="#" class="myButton1">Sign up<ArrowForwardIosIcon/></a>
+     <a href="#" class="myButton1" onClick={handleShow}>Sign up<ArrowForwardIosIcon/></a>
      <a href="#" class="myButton">Sign in<ArrowForwardIosIcon/></a>
      </div>
+     <SignUpForm show={showSignUp} handleClose={handleClose} />
     </div>
   );
 }
---
+++ Login ui-React/my-app/src/Components/Login/Login.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Modal, Button, Form } from 'react-bootstrap';
+
+const SignUpForm = ({ show, handleClose }) => {
+  return (
+    <Modal show={show} onHide={handleClose} centered>
+      <Modal.Header closeButton>
+        <Modal.Title>Sign Up</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <Form>
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </Modal.Body>
+    </Modal>
+  );
+};
+
+export default SignUpForm;
